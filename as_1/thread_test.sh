4de9dfce7975c11846d942acce876ac86cd2d883
dymnz <vicodin1123@gmail.com>
2017-10-11 19:21:24 +0800

    script update

--- as_1/thread_test.sh
+++ as_1/thread_test.sh
@@ -7,13 +7,13 @@ echo "testing: $a threads" | tee -a "./results/thread_test_sp3.txt"
 echo "running ans_2opt_sp3" | tee -a "./results/thread_test_sp3.txt"
 
 time(./ans_2opt_sp3 ./dis_dir/test11.dis ./in_dir/test11.in ./output_sp3 $a) | tee -a "./results/thread_test_sp3.txt"
-./verify_2opt ./dis_dir/test$a.dis ./output_sp3 | tee -a "./results/thread_test_sp3.txt"
+./verify_2opt ./dis_dir/test11.dis ./output_sp3 | tee -a "./results/thread_test_sp3.txt"
 
 a=`expr $a \* 2`
 done
 
 
-a=1
+a=122
 while [ "$a" -le 16 ]
 do 
 echo "testing: $a threads" | tee -a "./results/thread_test_sp5.txt"
