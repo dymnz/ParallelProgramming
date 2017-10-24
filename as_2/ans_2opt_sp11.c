a9218fd098cfb98cc7c579fa2675fce863b8d998
dymnz <vicodin1123@gmail.com>
2017-10-24 20:40:22 +0800

    route dist delta

--- as_2/ans_2opt_sp11.c
+++ as_2/ans_2opt_sp11.c
@@ -137,7 +137,7 @@ void two_opt(int start, int end) {
 	Race condition here. A better route may be overwriten.
 	*/
 	if (partial_new_distance < partial_original_distance) {
-		dist_type new_distance = get_route_distance(new_route_list);
+		dist_type new_distance = get_route_distance_delta(new_route_list, start, end);
 
 		pthread_rwlock_wrlock(&route_list_rwlock);
 		// Check if the route is really shorter to avoid race condition
@@ -227,7 +227,7 @@ void parallel_2opt() {
 	omp_set_num_threads(threads_to_use);
 
 	// Do until times up
-	
+
 	for (i = 1; i < go_flag & max_depth; ++i) {
 		for (depth = 1; go_flag & depth < num_city - i; ++depth)	 {
 			two_opt(i, i + depth);
@@ -248,277 +248,323 @@ void parallel_2opt() {
 #endif
 		}
 	}
+}
 
 
-	/*
-		Distance array is a serialized 2D-array with -1 as default value.
-		The distance is calculated when needed.
-	*/
-	dist_type get_city_distance(int index_1, int index_2) {
+/*
+	Distance array is a serialized 2D-array with -1 as default value.
+	The distance is calculated when needed.
+*/
+dist_type get_city_distance(int index_1, int index_2) {
 
 #ifdef KEEP_DIST_LIST
-		int array_index;
+	int array_index;
 
-		// The last node is equal to the first node
-		if (index_2 == num_city)
-			index_2 = 0;
+	// The last node is equal to the first node
+	if (index_2 == num_city)
+		index_2 = 0;
 
-		if (index_1 > index_2) {
-			array_index = num_city * (index_2)
-			              - (index_2 + 1) * index_2 / 2
-			              + (index_1 - index_2 - 1);
-		} else {
-			array_index = num_city * (index_1)
-			              - (index_1 + 1) * index_1 / 2
-			              + (index_2 - index_1 - 1);
-		}
+	if (index_1 > index_2) {
+		array_index = num_city * (index_2)
+		              - (index_2 + 1) * index_2 / 2
+		              + (index_1 - index_2 - 1);
+	} else {
+		array_index = num_city * (index_1)
+		              - (index_1 + 1) * index_1 / 2
+		              + (index_2 - index_1 - 1);
+	}
 
-		if (dist_list[array_index] == -1) {
-			dist_list[array_index] =
-			    distance(
-			        city_list[index_1].x,
-			        city_list[index_1].y,
-			        city_list[index_2].x,
-			        city_list[index_2].y);
-		}
+	if (dist_list[array_index] == -1) {
+		dist_list[array_index] =
+		    distance(
+		        city_list[index_1].x,
+		        city_list[index_1].y,
+		        city_list[index_2].x,
+		        city_list[index_2].y);
+	}
 
 #ifdef DEBUG
-		printf("Distance: %2d: %2d:%2d = %3f\n", array_index, index_1, index_2, dist_list[array_index]);
+	printf("Distance: %2d: %2d:%2d = %3f\n", array_index, index_1, index_2, dist_list[array_index]);
 #endif
 
-		return dist_list[array_index];
+	return dist_list[array_index];
 #else
-		return distance(
-		           city_list[index_1].x,
-		           city_list[index_1].y,
-		           city_list[index_2].x,
-		           city_list[index_2].y);
+	return distance(
+	           city_list[index_1].x,
+	           city_list[index_1].y,
+	           city_list[index_2].x,
+	           city_list[index_2].y);
 #endif
+}
+
+
+/*
+	Calculate the total route distance
+*/
+inline dist_type get_route_distance(int *route_index_list) {
+	int i;
+	int index_1, index_2;
+	dist_type distance_sum = 0.0;
+	for (i = 0; i < num_city; ++i) {
+		index_1 = route_index_list[i];
+		index_2 = route_index_list[i + 1];
+		distance_sum += get_city_distance(index_1, index_2);
+	}
+	return distance_sum;
+}
+
+/*
+	Calculate the total route distance when switching start and end index
+*/
+inline dist_type get_route_distance_delta(
+	int *route_index_list, 
+	int start, int end) 
+{
+	dist_type distance_sum = cache_route_distance;
+
+	distance_sum -= 
+		get_city_distance(
+			route_index_list[start - 1], 
+			route_index_list[start]);
+	distance_sum -= 
+		get_city_distance(
+			route_index_list[start], 
+			route_index_list[start + 1]);
+	distance_sum -= 
+		get_city_distance(
+			route_index_list[end - 1], 
+			route_index_list[end]);
+	distance_sum -= 
+		get_city_distance(
+			route_index_list[end], 
+			route_index_list[end + 1]);
+
+	distance_sum +=
+		get_city_distance(
+			route_index_list[start - 1], 
+			route_index_list[end]);
+	distance_sum +=
+		get_city_distance(
+			route_index_list[end], 
+			route_index_list[start + 1]);		
+	distance_sum +=
+		get_city_distance(
+			route_index_list[end - 1], 
+			route_index_list[start]);		
+	distance_sum +=
+		get_city_distance(
+			route_index_list[start], 
+			route_index_list[end + 1]);		
+	return distance_sum;
+}
+
+/*
+	Calculate the distance between index start and end.
+*/
+inline dist_type get_updated_route_distance(
+    int *route_index_list,
+    int start, int end)
+{
+	dist_type distance_sum;
+
+	distance_sum = get_city_distance(route_index_list[start - 1], route_index_list[start]);
+	distance_sum += get_city_distance(route_index_list[start], route_index_list[start + 1]);
+	distance_sum += get_city_distance(route_index_list[end - 1], route_index_list[end]);
+	distance_sum += get_city_distance(route_index_list[end], route_index_list[end + 1]);
+
+	return distance_sum;
+}
+
+
+/*
+	Print every node, including dummy last node
+*/
+void print_route() {
+	int i;
+	for (i = 0; i < num_city + 1; ++i) {
+		printf("%3d ", route_index_list[i] + 1);
+	}
+	printf("\n");
+}
+
+/*
+	Print every node to file, not including dummy last node
+*/
+void write_route(FILE * fpOutput) {
+	int i;
+
+	fprintf(fpOutput, "%f\n", get_route_distance(route_index_list));
+	for (i = 0; i < num_city; ++i) {
+		fprintf(fpOutput, "%d\n", route_index_list[i] + 1);
+	}
+}
+
+/*
+	Read original distance and route from file
+*/
+void *read_route(void *fp) {
+	FILE *fpRoute = (FILE *) fp;
+	route_index_list = (int *) malloc((num_city + 1) * sizeof(int));
+	if (route_index_list == NULL) {
+		printf("route_index_list malloc() error\n");
+		exit(56);
 	}
 
-
-	/*
-		Calculate the total route distance
-	*/
-	inline dist_type get_route_distance(int *route_index_list) {
-		int i;
-		int index_1, index_2;
-		dist_type distance_sum = 0.0;
-		for (i = 0; i < num_city; ++i) {
-			index_1 = route_index_list[i];
-			index_2 = route_index_list[i + 1];
-			distance_sum += get_city_distance(index_1, index_2);
-		}
-		return distance_sum;
+	fscanf(fpRoute, "%lf", &default_distance);
+	int index = 0, temp_i;
+	while (fscanf(fpRoute, "%d", &temp_i) != EOF) {
+		route_index_list[index++] = temp_i - 1;
 	}
 
-	/*
-		Calculate the distance between index start and end.
-	*/
-	inline dist_type get_updated_route_distance(
-	    int *route_index_list,
-	    int start, int end)
+	// Append the first node to the last
+	route_index_list[num_city] = route_index_list[0];
+
+	return NULL;
+}
+
+/*
+	Read city coords from file. The # of city line is processed in main()
+*/
+void *read_coord(void *fp) {
+	FILE *fpCoord = (FILE *) fp;
+	struct City temp_city = {
+		.index = -1,
+		.x = -1,
+		.y = -1,
+	};
+
+	// Init storage
+	city_list = (struct City *) malloc(num_city * sizeof(struct City));
+	if (city_list == NULL) {
+		printf("city_list malloc() error\n");
+		exit(34);
+	}
+
+	int index = 0;
+	while (fscanf(fpCoord, "%d %d %d",
+	              &(temp_city.index),
+	              &(temp_city.x),
+	              &(temp_city.y)) != EOF)
 	{
-		dist_type distance_sum;
+		--temp_city.index;
+		city_list[index] = temp_city;
 
-		distance_sum = get_city_distance(route_index_list[start - 1], route_index_list[start]);
-		distance_sum += get_city_distance(route_index_list[start], route_index_list[start + 1]);
-		distance_sum += get_city_distance(route_index_list[end - 1], route_index_list[end]);
-		distance_sum += get_city_distance(route_index_list[end], route_index_list[end + 1]);
-
-		return distance_sum;
+		++index;
 	}
 
+	return NULL;
+}
 
-	/*
-		Print every node, including dummy last node
-	*/
-	void print_route() {
-		int i;
-		for (i = 0; i < num_city + 1; ++i) {
-			printf("%3d ", route_index_list[i] + 1);
-		}
-		printf("\n");
+int main(int argc, char const * argv[])
+{
+	pthread_t fp_thread_list[2];
+
+	if (argc < 4) {
+		printf("not enough input\n");
+		exit(69);
 	}
 
-	/*
-		Print every node to file, not including dummy last node
-	*/
-	void write_route(FILE * fpOutput) {
-		int i;
+	// Get program start time
+	start_time = time(NULL);
+	go_flag = 1;
 
-		fprintf(fpOutput, "%f\n", get_route_distance(route_index_list));
-		for (i = 0; i < num_city; ++i) {
-			fprintf(fpOutput, "%d\n", route_index_list[i] + 1);
-		}
-	}
+	// Open file pointers
+	FILE *fpCoord, *fpRoute, *fpOutput;
+	fpCoord = fopen (argv[1], "r");
+	if (fpCoord == NULL) exit(1);
+	fpRoute = fopen (argv[2], "r");
+	if (fpRoute == NULL) exit(2);
+	fpOutput = fopen (argv[3], "w");
+	if (fpOutput == NULL) exit(3);
 
-	/*
-		Read original distance and route from file
-	*/
-	void *read_route(void *fp) {
-		FILE *fpRoute = (FILE *) fp;
-		route_index_list = (int *) malloc((num_city + 1) * sizeof(int));
-		if (route_index_list == NULL) {
-			printf("route_index_list malloc() error\n");
-			exit(56);
-		}
-
-		fscanf(fpRoute, "%lf", &default_distance);
-		int index = 0, temp_i;
-		while (fscanf(fpRoute, "%d", &temp_i) != EOF) {
-			route_index_list[index++] = temp_i - 1;
-		}
-
-		// Append the first node to the last
-		route_index_list[num_city] = route_index_list[0];
-
-		return NULL;
-	}
-
-	/*
-		Read city coords from file. The # of city line is processed in main()
-	*/
-	void *read_coord(void *fp) {
-		FILE *fpCoord = (FILE *) fp;
-		struct City temp_city = {
-			.index = -1,
-			.x = -1,
-			.y = -1,
-		};
-
-		// Init storage
-		city_list = (struct City *) malloc(num_city * sizeof(struct City));
-		if (city_list == NULL) {
-			printf("city_list malloc() error\n");
-			exit(34);
-		}
-
-		int index = 0;
-		while (fscanf(fpCoord, "%d %d %d",
-		              &(temp_city.index),
-		              &(temp_city.x),
-		              &(temp_city.y)) != EOF)
-		{
-			--temp_city.index;
-			city_list[index] = temp_city;
-
-			++index;
-		}
-
-		return NULL;
-	}
-
-	int main(int argc, char const * argv[])
-	{
-		pthread_t fp_thread_list[2];
-
-		if (argc < 4) {
-			printf("not enough input\n");
-			exit(69);
-		}
-
-		// Get program start time
-		start_time = time(NULL);
-		go_flag = 1;
-
-		// Open file pointers
-		FILE *fpCoord, *fpRoute, *fpOutput;
-		fpCoord = fopen (argv[1], "r");
-		if (fpCoord == NULL) exit(1);
-		fpRoute = fopen (argv[2], "r");
-		if (fpRoute == NULL) exit(2);
-		fpOutput = fopen (argv[3], "w");
-		if (fpOutput == NULL) exit(3);
-
-		// Use the thread_num in argument if available
-		if (argc > 4)
-			available_threads = atoi(argv[4]);
-		else
-			available_threads = THREAD_COUNT;
+	// Use the thread_num in argument if available
+	if (argc > 4)
+		available_threads = atoi(argv[4]);
+	else
+		available_threads = THREAD_COUNT;
 
 
 
-		printf("Working on %s\n", argv[1]);
+	printf("Working on %s\n", argv[1]);
 
-		// Read number of city
-		fscanf(fpCoord, "%d", &num_city);
+	// Read number of city
+	fscanf(fpCoord, "%d", &num_city);
 
 
 #ifdef KEEP_DIST_LIST
-		int i;
-		int num_edge = num_city  * (num_city - 1) / 2;	// C(N, 2)
-		// Distances are reset to -1
-		dist_list = (dist_type *) malloc(num_edge * sizeof(dist_type));
-		if (dist_list == NULL) {
-			printf("dist_list malloc() error\n");
-			exit(12);
-		}
-		for (i = 0; i < num_edge; ++i) {
-			dist_list[i] = -1;
-		}
+	int i;
+	int num_edge = num_city  * (num_city - 1) / 2;	// C(N, 2)
+	// Distances are reset to -1
+	dist_list = (dist_type *) malloc(num_edge * sizeof(dist_type));
+	if (dist_list == NULL) {
+		printf("dist_list malloc() error\n");
+		exit(12);
+	}
+	for (i = 0; i < num_edge; ++i) {
+		dist_list[i] = -1;
+	}
 #endif
 
 
 
-		// Read city coord and default route
-		pthread_create(&fp_thread_list[0], NULL, read_coord, (void*)fpCoord);
-		pthread_create(&fp_thread_list[1], NULL, read_route, (void*)fpRoute);
-		pthread_join(fp_thread_list[0], NULL);
-		pthread_join(fp_thread_list[1], NULL);
+	// Read city coord and default route
+	pthread_create(&fp_thread_list[0], NULL, read_coord, (void*)fpCoord);
+	pthread_create(&fp_thread_list[1], NULL, read_route, (void*)fpRoute);
+	pthread_join(fp_thread_list[0], NULL);
+	pthread_join(fp_thread_list[1], NULL);
 
-		// Init route_list_rwlock and go_flag_rwlock
-		pthread_rwlock_init(&route_list_rwlock, NULL);
-		pthread_rwlock_init(&go_flag_rwlock, NULL);
+	// Init route_list_rwlock and go_flag_rwlock
+	pthread_rwlock_init(&route_list_rwlock, NULL);
+	pthread_rwlock_init(&go_flag_rwlock, NULL);
 
-		// Init the cache_route_distance
-		cache_route_distance = get_route_distance(route_index_list);
+	// Init the cache_route_distance
+	cache_route_distance = get_route_distance(route_index_list);
 
 #ifdef VERBOSE
-		printf("Original route:\n");
-		print_route();
-		printf("Original route distance: %lf\n", get_route_distance(route_index_list));
+	printf("Original route:\n");
+	print_route();
+	printf("Original route distance: %lf\n", get_route_distance(route_index_list));
 #endif
 
-		printf("Balanced search space division/Proper/Distance cache/Partial distance compare:\n");
-		parallel_2opt();
+	printf("Balanced search space division/Proper/Distance cache/Partial distance compare:\n");
+	parallel_2opt();
 
 #ifdef VERBOSE
-		print_route();
+	print_route();
 #endif
 
-		printf("Final route distance: %lf\n", get_route_distance(route_index_list));
+	printf("Final route distance: %lf\n", get_route_distance(route_index_list));
 
-		// Write to file
-		write_route(fpOutput);
+	// Write to file
+	write_route(fpOutput);
 
 #ifdef ENABLE_2OPT_COUNTER
-		int i;
-		int total_opt_count = 0;
-		for (i = 0; i < available_threads; ++i)
-			total_opt_count += opt_counter_list[i];
+	int i;
+	int total_opt_count = 0;
+	for (i = 0; i < available_threads; ++i)
+		total_opt_count += opt_counter_list[i];
 
-		printf("call: %7d swap: %7d %%: %.2f race: %3d %%: %.2f avg_swap_length: %.2lf avg_dist_dec: %.2lf\n",
-		       total_opt_count,
-		       swap_counter,
-		       swap_counter * 100.0f / total_opt_count,
-		       race_cond_counter,
-		       race_cond_counter * 100.0f / total_opt_count,
-		       total_swap_length / swap_counter,
-		       total_reduced_distance / swap_counter);
+	printf("call: %7d swap: %7d %%: %.2f race: %3d %%: %.2f avg_swap_length: %.2lf avg_dist_dec: %.2lf\n",
+	       total_opt_count,
+	       swap_counter,
+	       swap_counter * 100.0f / total_opt_count,
+	       race_cond_counter,
+	       race_cond_counter * 100.0f / total_opt_count,
+	       total_swap_length / swap_counter,
+	       total_reduced_distance / swap_counter);
 #endif
 
 
-		/* Cleanup */
-		fclose(fpOutput);
-		fclose(fpCoord);
-		fclose(fpRoute);
+	/* Cleanup */
+	fclose(fpOutput);
+	fclose(fpCoord);
+	fclose(fpRoute);
 
-		free(city_list);
-		free(route_index_list);
+	free(city_list);
+	free(route_index_list);
 
-		pthread_rwlock_destroy(&route_list_rwlock);
-		pthread_rwlock_destroy(&go_flag_rwlock);
+	pthread_rwlock_destroy(&route_list_rwlock);
+	pthread_rwlock_destroy(&go_flag_rwlock);
 
-		return 0;
-	}
+	return 0;
+}
