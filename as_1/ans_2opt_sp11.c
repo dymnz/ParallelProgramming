97f135bf907baad1461bd894180261e1220d2eda
dymnz <vicodin1123@gmail.com>
2017-10-24 21:40:32 +0800

    ha

--- as_1/ans_2opt_sp11.c
+++ as_1/ans_2opt_sp11.c
@@ -131,7 +131,7 @@ void two_opt(int start, int end) {
 	*/
 	if (partial_new_distance < partial_original_distance) {
 		int *new_route_list = (int *) malloc((num_city + 1) * sizeof(int));
-		
+
 		pthread_rwlock_rdlock(&route_list_rwlock);
 		// Copy
 		memcpy(new_route_list,
@@ -140,9 +140,17 @@ void two_opt(int start, int end) {
 		for (i = 0; i < end - start + 1; ++i) {
 			new_route_list[start + i] = route_index_list[end - i];
 		}
+		dist_type new_distance = get_route_distance_delta(route_index_list, start, end);
+		dist_type new_distance_2 = get_route_distance(route_index_list, start, end);
+
+		if (fabs(new_distance-new_distance_2)>1) {
+			printf("shit\n");
+			exit(1111);
+		}
+
 		pthread_rwlock_unlock(&route_list_rwlock);
 
-		dist_type new_distance = get_route_distance_delta(route_index_list, start, end);
+		
 
 		pthread_rwlock_wrlock(&route_list_rwlock);
 		// Check if the route is really shorter to avoid race condition
