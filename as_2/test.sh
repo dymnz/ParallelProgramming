615ee6a4ace03ebf5f50a94539390bf9ba711f43
vicodin1123 <vicodin1123@gmail.com>
2017-10-25 16:47:45 +0800

    test.sh

--- as_2/test.sh
+++ as_2/test.sh
@@ -4,6 +4,6 @@
 echo "testing: $2 threads" | tee -a "./results/thread_test_omp$1_$2.txt"
 echo "running ans_2opt_omp$1" | tee -a "./results/thread_test_omp$1_$2.txt"
 
-time(./ans_2opt_omp$1 ../as_1/dis_dir/test11.dis ../as_1/in_dir/test11.in ./output_omp$1_$2 $2) | tee -a "./results/thread_test_omp$1_$2.txt"
-./verify_2opt ../as_1/dis_dir/test11.dis ./output_omp$1_$2 | tee -a "./results/thread_test_omp$1_$2.txt"
+time(./ans_2opt_omp$1 ../as_1/dis_dir/test$3.dis ../as_1/in_dir/test$3.in ./output_omp$1_$2 $2) | tee -a "./results/thread_test_omp$1_$2.txt"
+./verify_2opt ../as_1/dis_dir/test$3.dis ./output_omp$1_$2 | tee -a "./results/thread_test_omp$1_$2.txt"
 
