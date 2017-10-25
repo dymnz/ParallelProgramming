9192aaef4fcf0e94015ea59a40b861b623d64847
vicodin1123 <vicodin1123@gmail.com>
2017-10-25 16:33:45 +0800

    test fix

--- as_2/test.sh
+++ as_2/test.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 
-echo "testing: $2 threads" | tee -a "./results/thread_test_sp$1_$2.txt"
-echo "running ans_2opt_sp$1" | tee -a "./results/thread_test_sp$1_$2.txt"
+echo "testing: $2 threads" | tee -a "./results/thread_test_omp$1_$2.txt"
+echo "running ans_2opt_omp$1" | tee -a "./results/thread_test_omp$1_$2.txt"
 
-time(./ans_2opt_sp$1 ../as_1/dis_dir/test11.dis ../as_1/in_dir/test11.in ./output_sp$1_$2 $2) | tee -a "./results/thread_test_sp$1_$2.txt"
-./verify_2opt ../as_1/dis_dir/test11.dis ./output_sp$1_$2 | tee -a "./results/thread_test_sp$1_$2.txt"
+time(./ans_2opt_omp$1 ../as_1/dis_dir/test11.dis ../as_1/in_dir/test11.in ./output_omp$1_$2 $2) | tee -a "./results/thread_test_omp$1_$2.txt"
+./verify_2opt ../as_1/dis_dir/test11.dis ./output_omp$1_$2 | tee -a "./results/thread_test_omp$1_$2.txt"
 
