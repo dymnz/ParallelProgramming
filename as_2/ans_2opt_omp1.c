44a71881e2a01ede0f8e7ef8b032dfb64542c870
vicodin1123 <vicodin1123@gmail.com>
2017-10-25 17:00:40 +0800

    infinite loop

--- as_2/ans_2opt_omp1.c
+++ as_2/ans_2opt_omp1.c
@@ -168,63 +168,65 @@ void parallel_2opt() {
 
 	#pragma omp parallel
 	{
-		for (depth = 1; go_flag && depth < max_depth; ++depth) {
-			for (i = 1; go_flag && i < num_city - depth; ) {
-				// Spawn available_threads thread with task,
-				// increment i accordingly
-				#pragma omp single
-				for (task_i = 0; task_i < available_threads; ++task_i, ++i) {
+		while (go_flag) {
+			for (depth = 1; go_flag && depth < max_depth; ++depth) {
+				for (i = 1; go_flag && i < num_city - depth; ) {
+					// Spawn available_threads thread with task,
+					// increment i accordingly
+					#pragma omp single
+					for (task_i = 0; task_i < available_threads; ++task_i, ++i) {
 #ifdef VERBOSE
-					printf("Create thread: %3d s: %8d e: %8d\n",
-					       task_i, i, i + depth);
+						printf("Create thread: %3d s: %8d e: %8d\n",
+						       task_i, i, i + depth);
 #endif
-					#pragma omp task
+						#pragma omp task
+						{
+							thread_submit_list[omp_get_thread_num()].start = i;
+							thread_submit_list[omp_get_thread_num()].end = i + depth;
+							thread_submit_list[omp_get_thread_num()].distance_reduced =
+							    two_opt_check(i, i + depth);
+						}
+					}
+					#pragma omp taskwait
+
+					// Main thread check the delta distance produced by each thread.
+					// Execute 2opt swap at start/edn with the best reduced distance
+					#pragma omp single
 					{
-						thread_submit_list[omp_get_thread_num()].start = i;
-						thread_submit_list[omp_get_thread_num()].end = i + depth;
-						thread_submit_list[omp_get_thread_num()].distance_reduced =
-						    two_opt_check(i, i + depth);
-					}
-				}
-				#pragma omp taskwait
+						// Find the best distance delta
+						best_thread_num = 0;
+						for (task_i = 1; task_i < available_threads; ++task_i)
+							if (thread_submit_list[task_i].distance_reduced <
+							        thread_submit_list[best_thread_num].distance_reduced)
+								best_thread_num = task_i;
 
-				// Main thread check the delta distance produced by each thread.
-				// Execute 2opt swap at start/edn with the best reduced distance
-				#pragma omp single
-				{
-					// Find the best distance delta
-					best_thread_num = 0;
-					for (task_i = 1; task_i < available_threads; ++task_i)
-						if (thread_submit_list[task_i].distance_reduced <
-						        thread_submit_list[best_thread_num].distance_reduced)
-							best_thread_num = task_i;
+						// Finally, execute 2opt swap
+						if (thread_submit_list[best_thread_num].distance_reduced > 0) {
+							two_opt_swap(
+							    thread_submit_list[best_thread_num].start,
+							    thread_submit_list[best_thread_num].end);
+							swap_counter++;
+							total_swap_length +=
+							    thread_submit_list[best_thread_num].end -
+							    thread_submit_list[best_thread_num].start + 1;
+							total_reduced_distance +=
+							    thread_submit_list[best_thread_num].distance_reduced;
+						}
 
-					// Finally, execute 2opt swap
-					if (thread_submit_list[best_thread_num].distance_reduced > 0) {
-						two_opt_swap(
-						    thread_submit_list[best_thread_num].start,
-						    thread_submit_list[best_thread_num].end);
-						swap_counter++;
-						total_swap_length +=
-						    thread_submit_list[best_thread_num].end -
-						    thread_submit_list[best_thread_num].start + 1;
-						total_reduced_distance +=
-						    thread_submit_list[best_thread_num].distance_reduced;
-					}
-
-					// Check time
-					go_flag = time(NULL) <
-					          start_time + SECONDS_TO_WAIT - SECONDS_BUFFER;
+						// Check time
+						go_flag = time(NULL) <
+						          start_time + SECONDS_TO_WAIT - SECONDS_BUFFER;
 
 #ifdef PRINT_STATUS
-					if ((time(NULL) - start_time) % 30 == 0 &&
-					        (time(NULL) - start_time) > 0) {
-						printf("Distance @ %2lu:%02lu = %lf\n",
-						       (unsigned long)(time(NULL) - start_time) / 60,
-						       (unsigned long)(time(NULL) - start_time) % 60,
-						       cache_route_distance);
-					}
+						if ((time(NULL) - start_time) % 30 == 0 &&
+						        (time(NULL) - start_time) > 0) {
+							printf("Distance @ %2lu:%02lu = %lf\n",
+							       (unsigned long)(time(NULL) - start_time) / 60,
+							       (unsigned long)(time(NULL) - start_time) % 60,
+							       cache_route_distance);
+						}
 #endif
+					}
 				}
 			}
 		}
